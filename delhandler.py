f8b76288d468e4b279c86c53acfd6191e8b1f5ae
surajmsd <48248895+surajmsd@users.noreply.github.com>
2019-03-08 17:52:34 +0530

    Update delhandler.py

--- delhandler.py
+++ delhandler.py
@@ -1,1 +1,18 @@
+import tornado.web
+from customer import customers
+import json
 
+
+class DelHandler(tornado.web.RequestHandler):
+    def initialize(self,customer):
+        self.customer = customer
+        
+    def get(self):
+        phone = self.get_argument('phone')
+        result = self.customer.del_phone(phone)
+        if result:
+            self.write("Deleted Customer phone: {0} successfully".format(phone))
+            self.set_status(200)
+        else:
+            self.write("Customer '{0}' not found".format(phone))
+            self.set_status(404)
